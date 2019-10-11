87a1ffb4178d53d9bb24b1d83e076f741a669641
Candy Lin <lincandy.yite@gmail.com>
2019-10-10 17:00:43 -0700

    Add Dropdown Text: Test add dropdown text for each question

---
+++ js/for-you.js
@@ -0,0 +1,14 @@
+var dropButtons = document.getElementsByClassName("drop-btn");
+
+// Will toggle all dropdown texts in the same node
+function toggleDropDown(event){
+	var texts = event.srcElement.parentNode.getElementsByClassName("dropdown-text");
+
+	for (i = 0; i < texts.length; i++){
+		texts[i].classList.toggle("hide");
+	}
+}
+
+for (i = 0; i < dropButtons.length; i++){
+	dropButtons[i].addEventListener("click",toggleDropDown);
+}
